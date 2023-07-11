3fdc2a66261ee3ac555c8fede2296c7993230d5a
ms <mateusz@>
2023-07-11 13:23:31 +0200

    code zad1

---
+++ App.js
@@ -0,0 +1,29 @@
+class App extends React.Component {
+  state = {
+    options: ["1","2","3"],
+    option: null,
+    
+  }
+
+  handleWrozba =()=>{
+    const los = Math.floor(Math.random()*this.state.options.length);
+    this.setState({
+      option: this.state.options[los]
+    })
+  }
+
+  render() {
+    return ( 
+      <div>
+        <button onClick={this.handleWrozba}>Zobacz wróżbę</button>
+        <br></br>
+        <input></input>
+        <button>Dodaj wróżbę</button>
+        {this.state.option ? <h1>{this.state.option}</h1> : null}
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render( < App / > , document.getElementById('root'))
